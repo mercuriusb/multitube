14f3ba0474d3500a723bc838de7b964baef1d0d3
mercuriusb <merkur42@googlemail.com>
2022-05-28 22:46:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Control from "./components/control";
 function App() {
   const [urls,setUrls] = useState([
     {"url":"https://www.youtube.com/watch?v=FVjTSBefDjU","desc":"Main"}, 
-    {"url":"https://www.youtube.com/watch?v=xsF3QBpvIvE","desc":"Onboard #1 | Porsche 911 GT3 R | Manthey-Racing"}, 
+    //{"url":"https://www.youtube.com/watch?v=xsF3QBpvIvE","desc":"Onboard #1 | Porsche 911 GT3 R | Manthey-Racing"}, 
     {"url":"https://www.youtube.com/watch?v=ZWvFkdjh_ko","desc":"Onboard #3 | Mercedes-AMG GT3 | Mercedes-AMG Team Getspeed"}, 
     {"url":"https://www.youtube.com/watch?v=cemGzGeo4Hs","desc":"Onboard #5 | Audi R8 LMS GT3 | Scherer Sport Team Phoenix"}, 
     {"url":"https://www.youtube.com/watch?v=eC2xP2B8Bn8","desc":"Onboard #12 | Mercedes-AMG GT3 | MercedesAMG Team Bilstein"}, 
@@ -18,7 +18,7 @@ function App() {
     {"url":"https://www.youtube.com/watch?v=Leq41aBGhAE","desc":"Onboard #44 | Porsche 911 GT3 R | Falken Motorsports"}, 
     {"url":"https://www.youtube.com/watch?v=cgs3tF-dN78","desc":"Onboard #72 | BMW M4 GT3 | BMW Junior Team"},
     {"url":"https://www.youtube.com/watch?v=FoYaUBPYZow","desc":"Onboard #98 | BMW M4 GT3 | ROWE Racing"}, 
-    {"url":"https://www.youtube.com/watch?v=IqruW4FW0rM","desc":"Onboard #160 | KTM X-BOW GTX | Teichmann Racing"}, 
+    //{"url":"https://www.youtube.com/watch?v=IqruW4FW0rM","desc":"Onboard #160 | KTM X-BOW GTX | Teichmann Racing"}, 
     {"url":"https://www.youtube.com/watch?v=EmCbafg0tZE","desc":"Onboard #162 | KTM X-BOW GTX | Teichmann Racing"}, 
     {"url":"https://www.youtube.com/watch?v=nUWd6IIJ0F8","desc":"Onboard #831 | Hyundai i30 N TCR | Hyundai Motorsport N "}
   ]);
